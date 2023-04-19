f748c893321148dcaed7b0a8707c8b9541bb6d8a
idannydev <benitezdaniel6@gmail.com>
2023-04-18 22:53:19 -0400

    [FIX] Added data stack property

--- client/src/containers/WebhooksList.jsx
+++ client/src/containers/WebhooksList.jsx
@@ -28,7 +28,7 @@ function WebhooksList() {
             />
             <Webhook
                 title={"Grouped bar chart"}
-                webhookUrl="http://localhost:5173/bar/grouped?title=Sample%20Chart&labels=Label%201;Label%202;Label%203&dataSetLabels=Label%201.1;Label%202.1;Label%203.1&dataSetValues=4,9,3;5,7,5;8,4,7&dataSetColors=de6b48;e5b181;606e8c&dataSetStack=0;1;2"
+                webhookUrl="http://localhost:5173/bar/grouped?title=Sample%20Chart&labels=Label%201;Label%202;Label%203&dataSetLabels=Label%201.1;Label%202.1;Label%203.1&dataSetValues=4,9,3;5,7,5;8,4,7&dataSetColors=de6b48;e5b181;606e8c&dataSetStack=1;2;2"
                 width={400}
                 height={200}
                 className="webhook-item"
